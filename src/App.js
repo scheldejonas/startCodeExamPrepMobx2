e456f2e1789214d5b7e1e36664d9b117489542e6
Schelde Jonas <scheldejonas@gmail.com>
2017-04-12 19:31:19 +0200

    Updated SP7 -> first 3 tasks

--- src/App.js
+++ src/App.js
@@ -5,9 +5,32 @@ import info from "./dataModel"
 class App extends Component {
  
   render() {
-    var studentInfo = info.studentsInfo;
-    var headerCount = studentInfo.headers.length;
-    var studentCount = studentInfo.students.length;
+    const studentInfo = info.studentsInfo;
+    const headerCount = studentInfo.headers.length;
+    const studentCount = studentInfo.students.length;
+    console.log('...Data is fetched for table with the course count of: ' + headerCount + ', and also student count: ' + studentCount + '.');
+    console.log('...Data Headers is fetched as: ');
+    console.log(studentInfo.headers);
+    const headerRowMapped = studentInfo.headers.map(function(course, index) {
+        return (<th key={course.courseId}>{course.courseName}</th>);
+      }
+    );
+    console.log('...HeaderRowMapped is done preparing and are looking like this:');
+    console.log(headerRowMapped);
+    const studentCellsMapped = studentInfo.students.map(
+      (student, index) => {
+        return (
+          <tr key={student.studentId}>
+            <td>{student.name}</td>
+            {student.grades.map((grade, index) => {
+              return <td key={index}>{grade.grade}</td>;
+            })}
+          </tr>
+        );
+      }
+    );
+    console.log('...StudentCellsMapped is done preparing and are looking like this:');
+    console.log(studentCellsMapped);
     return (
       <div className="App">
         <div className="App-header">
@@ -19,9 +42,15 @@ class App extends Component {
           <p>One with all the Students, whis is a total of ({studentInfo.students.length})</p>
           <p>Use the empty table below, or move it (you must eventually) into a separate component</p>
           <table className="table">
-            <thead>      
+            <thead>
+              <tr>
+                <th></th>
+                {headerRowMapped}
+                <th>Average</th>
+              </tr>
             </thead>
             <tbody>
+              {studentCellsMapped}
             </tbody>
           </table>
         </div>
--- src/dataModel.js
+++ src/dataModel.js
@@ -14,7 +14,8 @@ class StudentInfoFactory {
     this._studentsInfo.headers = [
       { courseId: 1000, courseName: "Basic Programming" },
       { courseId: 1001, courseName: "Advanced Programming" },
-      { courseId: 1003, courseName: "DataBase Intro" }];
+      { courseId: 1003, courseName: "DataBase Intro" }
+    ];
     this._studentsInfo.students = [];
     this._studentsInfo.students.push({ studentId: 100, name: "Peter Hansen", grades: [{ grade: "10" }, { grade: "12" }, {}] });
     this._studentsInfo.students.push({ studentId: 101, name: "Jan Olsen", grades: [{ grade: "7" }, { grade: "10" }, {}] });
